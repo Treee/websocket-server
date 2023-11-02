12781215069cf16a539ce4798b59db342f9c9e38
Treee <itsatreee.twitch@gmail.com>
2023-11-02 09:35:28 -0600

    adding simple webserver on port 3000 to test

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
 import MyWebSocketServer from "./src/websocket-server.js";
 
-const mySocketServer = new MyWebSocketServer();
-mySocketServer.startServer();
+// const mySocketServer = new MyWebSocketServer();
+// mySocketServer.startServer();
+
+// const express = require("express");
+import express from "express";
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Application started and Listening on port 3000");
+});
+
+const text = `
+<!DOCTYPE html>
+<html>
+  <head> </head>
+  <body>
+    <div class="center_div">
+      <h1>Hello World!</h1>
+      <p>This example contains some advanced CSS methods you may not have learned yet. But, we will explain these methods in a later chapter in the tutorial.</p>
+    </div>
+  </body>
+</html>
+`;
+
+app.get("/", (req, res) => {
+  res.send(text);
+});
